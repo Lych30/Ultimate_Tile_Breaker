ecb7622b6498ed66e1c3bc39d25134605828a01f
EstebanNunez <esteban78570@gmail.com>
2021-01-16 19:57:37 +0100

    init LifeSystem

---
+++ Tile_Breaker/Assets/Scripts/LifesSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifesSystem : MonoBehaviour
+{
+    private static LifesSystem instance;
+
+    private int lifes = 3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static LifesSystem GetInstance()
+    {
+        return instance;
+    }
+
+    public void LoseLife()
+    {
+        lifes--;
+    }
+}
