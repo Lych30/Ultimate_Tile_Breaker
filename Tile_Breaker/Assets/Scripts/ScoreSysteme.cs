1a034fe2d9125c5ecfdd3e93380590c0edf5e6a5
EstebanNunez <esteban78570@gmail.com>
2021-01-16 18:55:00 +0100

    update Score

--- Tile_Breaker/Assets/Scripts/ScoreSysteme.cs
+++ Tile_Breaker/Assets/Scripts/ScoreSysteme.cs
@@ -1,22 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreSysteme : MonoBehaviour
 {
-    [SerializeField] private int score;
-
-
+    [SerializeField] private Text scoreText;
+    
+    private int score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        RefreshScore();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            AddScore(100);
+        }
     }
+
+    private void RefreshScore()
+    {
+        scoreText.text = scoreText.text.Remove(7) + " " + score;
+    }
+
+
+    public void AddScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        RefreshScore();
+    }
+
 }
